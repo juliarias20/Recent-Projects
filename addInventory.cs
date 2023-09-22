49c14ec8345ce3ddb011e5519e994a5984614cc4
juliarias20 <128403441+juliarias20@users.noreply.github.com>
2023-09-21 23:29:18 -0700

    Add files via upload

---
+++ addInventory.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class addInventory : MonoBehaviour
+{
+    public List<string> itemList = new List<string>();
+    public GameObject Canvas;
+    public GameObject StoneCanvas;
+    public GameObject WoodenStickCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas.SetActive(false);
+        StoneCanvas.SetActive(false);
+        WoodenStickCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void Sword()
+    {
+        Canvas.SetActive(true);
+    }
+
+    public void StoneSword()
+    {
+        StoneCanvas.SetActive(true);
+    }
+
+    public void Stick()
+    {
+        WoodenStickCanvas.SetActive(true);
+    }
+    //the code can be duplicated and modified to fit into whichever item the player is picking up.
+
+}
